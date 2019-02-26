5d588c41de7f9ba4523f5d800801941467bc6b7b
Arman Siddique <arman.siddique887@gmail.com>
2019-02-26 01:59:44 -0500

    Implemented running the DFA

--- dfa.py
+++ dfa.py
@@ -42,21 +42,38 @@ def gen_dfa_classifier(b, m, initial, repeating):
 
     return dfa
 
+def compute_dfa(dfa, x):
+    """Uses the DFA to compute (x mod m) in base b"""
+    states = [(0, 0)]
+
+    for d in x:
+        s = states[-1]
+        states.append(dfa[s[0]][s[1]][d])
+
+    return states
 
 def main(args):
     b = int(args[1])
     m = int(args[2])
     i = int(args[3])
-    x = args[3]
+    x = list(map(int, args[4:]))
 
     initial, repeating = get_pattern(b, m)
     print(initial)
     print(repeating)
 
     dfa = gen_dfa_classifier(b, m, initial, repeating)
-    for i in range(len(dfa)):
-        for j in range(len(dfa[i])):
-            print("(%d, %d) %s" % (i, j, str(dfa[i][j])))
+    for j in range(len(dfa)):
+        for k in range(len(dfa[j])):
+            print("(%d, %d) %s" % (j, k, str(dfa[j][k])))
+    print(len(dfa) * len(dfa[0]))
 
+    states = compute_dfa(dfa, x)
+    print(str(states))
+
+    if states[-1][1] == i:
+        print("accept")
+    else:
+        print("reject")
 
 main(sys.argv)
